3f07bd12ecb338c6b243caf7f4f7e7547912399d
mckk84 <mckk84@users.noreply.github.com>
2025-02-22 20:28:02 +0530

    admin login

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -1,13 +1,88 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+/**
+ * Class : Admin (LoginController)
+ * Admin class to control to authenticate user credentials and starts user's session.
+ */
+class Admin extends CI_Controller
+{
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('login_model');
+    }
 
-	public function index()
-	{
-		$data['page_title'] = 'Rock N Roll Bike Rentals | Admin';
-        $this->load->view('layout_admin/header', $data);
-        $this->load->view('backend/home', $data);
-        $this->load->view('layout_admin/footer');
-	}
-}
+    /**
+     * Index Page for this controller.
+     */
+    public function index()
+    {
+        $isLoggedIn = $this->session->userdata('isLoggedIn');
+        
+        if(!isset($isLoggedIn) || $isLoggedIn != TRUE)
+        {
+            $data['page_title'] = 'Rock N Roll Bike Rentals | Admin';
+	        $this->load->view('backend/login', $data);
+        }
+        else
+        {
+            redirect('admin/Dashboard');
+        }
+    }
+    
+    
+    /**
+     * This function used to logged in user
+     */
+    public function loginMe()
+    {
+        $this->load->library('form_validation');
+        
+        $this->form_validation->set_rules('username', 'Username', 'required|max_length[128]|trim');
+        $this->form_validation->set_rules('password', 'Password', 'required|max_length[10]');
+        
+        if($this->form_validation->run() == FALSE)
+        {
+            $this->index();
+        }
+        else
+        {
+            $username = strtolower($this->security->xss_clean($this->input->post('username')));
+            $password = $this->input->post('password');
+            
+            $result = $this->login_model->loginMe($username, $password);
+
+            //pre($result); die;
+            
+            if (!empty($result))
+            {
+                $sessionArray = array( 'userId'=>$result->userId,
+                                        'name'=>$result->name,
+                                        'isAdmin'=>$result->isAdmin,
+                                        'isLoggedIn' => TRUE
+                                );
+
+                $this->session->set_userdata($sessionArray);
+
+                unset($sessionArray['userId'], $sessionArray['isLoggedIn']);
+
+                $loginInfo = array("userId"=>$result->userId, "sessionData" => json_encode($sessionArray), "machineIp"=>$_SERVER['REMOTE_ADDR'], "userAgent"=>getBrowserAgent(), "agentString"=>$this->agent->agent_string(), "platform"=>$this->agent->platform());
+                
+                redirect('admin/Dashboard');
+            }
+            else
+            {
+                $this->session->set_flashdata('error', 'username or password incorrect');
+                redirect('admin');
+            }
+        }
+    }
+
+ }
+
+?>
+  
