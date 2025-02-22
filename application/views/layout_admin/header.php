eb6d3d88462722c0037a974f3ca1ffef763452a9
mckk84 <mckk84@users.noreply.github.com>
2025-02-22 21:54:01 +0530

    admin

--- application/views/layout_admin/header.php
+++ application/views/layout_admin/header.php
@@ -199,7 +199,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="<?=base_url()?>assets/admin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <i class="bi bi-person"></i>
             <span class="d-none d-md-block dropdown-toggle ps-2"><?=$user['name']?></span>
           </a><!-- End Profile Iamge Icon -->
 
