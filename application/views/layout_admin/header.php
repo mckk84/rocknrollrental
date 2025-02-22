f7fc8c0ac30db463bfd351fd58d4974fb1a09b0b
mckk84 <mckk84@users.noreply.github.com>
2025-02-22 19:39:22 +0530

    url

--- application/views/layout_admin/header.php
+++ application/views/layout_admin/header.php
@@ -6,24 +6,24 @@
 
   <title><?=$page_title?></title>
     <!-- Favicons -->
-  <link href="<?=base_url()?>/admin/assets/img/favicon.png" rel="icon">
-  <link href="<?=base_url()?>/admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?=base_url()?>assets/admin/assets/img/favicon.png" rel="icon">
+  <link href="<?=base_url()?>assets/admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?=base_url()?>admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?=base_url()?>/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?=base_url()?>/admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?=base_url()?>/admin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="<?=base_url()?>/admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="<?=base_url()?>/admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?=base_url()?>/admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="<?=base_url()?>/admin/assets/css/style.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/admin/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
@@ -41,7 +41,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="<?=base_url()?>/admin/assets/img/logo.png" alt="">
+        <img src="<?=base_url()?>assets/admin/assets/img/logo.png" alt="">
         <span class="d-none d-lg-block">NiceAdmin</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -156,7 +156,7 @@
 
             <li class="message-item">
               <a href="#">
-                <img src="<?=base_url()?>/admin/assets/img/messages-1.jpg" alt="" class="rounded-circle">
+                <img src="<?=base_url()?>assets/admin/assets/img/messages-1.jpg" alt="" class="rounded-circle">
                 <div>
                   <h4>Maria Hudson</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -170,7 +170,7 @@
 
             <li class="message-item">
               <a href="#">
-                <img src="<?=base_url()?>/admin/assets/img/messages-2.jpg" alt="" class="rounded-circle">
+                <img src="<?=base_url()?>assets/admin/assets/img/messages-2.jpg" alt="" class="rounded-circle">
                 <div>
                   <h4>Anna Nelson</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -184,7 +184,7 @@
 
             <li class="message-item">
               <a href="#">
-                <img src="<?=base_url()?>/admin/assets/img/messages-3.jpg" alt="" class="rounded-circle">
+                <img src="<?=base_url()?>assets/admin/assets/img/messages-3.jpg" alt="" class="rounded-circle">
                 <div>
                   <h4>David Muldon</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -207,7 +207,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="<?=base_url()?>/admin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <img src="<?=base_url()?>assets/admin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
           </a><!-- End Profile Iamge Icon -->
 
