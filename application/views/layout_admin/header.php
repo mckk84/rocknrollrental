254ba6955b823b65944116760ea0331d465a68ac
mckk84 <mckk84@users.noreply.github.com>
2025-02-23 11:55:18 +0530

    Update header.php

--- application/views/layout_admin/header.php
+++ application/views/layout_admin/header.php
@@ -220,13 +220,13 @@
       </li><!-- End Components Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <a class="nav-link collapsed" data-bs-target="#Bikes-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Bikes</span><i class="bi bi-list ms-auto"></i>
         </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <ul id="Bikes-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="components-alerts.html">
-              <i class="bi bi-bycycle"></i><span>Bikes</span>
+              <i class="bi bi-bicycle"></i><span>Bikes</span>
             </a>
           </li>
           <li>
