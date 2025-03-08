1b6f073f09b030421086f87144a777ae9237f600
mckk84 <mckk84@users.noreply.github.com>
2025-03-08 19:33:59 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -175,7 +175,7 @@ class Checkout extends CI_Controller {
 					{
 						$data['weekend'] = 1;
 					}
-					$res = $this->publicholidays_model->checkRecordExists(dateformatdb($data['pickup_date']));
+					$res = $this->publicholidays_model->checkRecordExists($data['pickup_date']);
 					if( $res )
 					{
 						$data['public_holiday'] = 1;
