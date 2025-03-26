6b784b664fa62323c98869bc1583796ebf171931
mckk84 <mckk84@users.noreply.github.com>
2025-03-26 16:49:31 +0530

    ac

--- application/views/front/account.php
+++ application/views/front/account.php
@@ -16,17 +16,33 @@
         <!--breadcrumb section end-->
 
         <!--product details tab-section-->
-        <section class="product-details-tab-section pb-120 pt-60">
+        <section class="product-details-tab-section pb-60 pt-60">
             <div class="container">
                 <div class="row">
-                    <div class="col-xl-3 col-md-4 col-sm-12 mb-2">
-                        <div class="product-details-tab-content bg-white border">
+                    <div class="d-none d-md-block col-xl-3 col-md-4 col-sm-12 mb-2">
+                        <div class="bg-white border">
                             <ul class="account_tabs nav nav-tabs py-3 px-4 border-0 mt-0">
-                                <li class="d-inline-block px-1 m-1"><a href="#basicinfo" id="tab_basicinfo" data-bs-toggle="tab" class="active"><i class="fa fa-user me-2"></i>Basic Info</a></li>
+                                <li class="d-inline-block px-1 m-1">
+                                    <a href="#basicinfo" id="tab_basicinfo" data-bs-toggle="tab" class="active"><i class="fa fa-user me-2"></i>Basic Info</a></li>
                                 <li class="d-inline-block px-1 m-1"><a href="#changepassword" id="tab_changepassword" data-bs-toggle="tab"><i class="fa fa-gear me-2"></i>Change Password</a></li>
                                 <li class="d-inline-block px-1 m-1"><a href="#rentals" data-bs-toggle="tab" id="tab_rentals"><i class="fa fa-book me-2"></i>Rentals</a></li>
+                                <li class="d-inline-block px-1 m-1"><a href="<?=base_url('/Auth/signoff')?>"><i class="fa fa-sign-out-alt me-2"></i>Logout</a></li>
                             </ul>
                         </div>
+                    </div>
+                    <div class="d-none dsm-block col-sm-12 dropdown">
+                        <div class="w-100 d-flex justify-content-between align-items-center d-md-none px-4 py-4">
+                            <span class="d-inline-block fs-4 mb-0">My Account </span>
+                            <button type="button" class="dropdown-menu-toggle text-muted d-md-none btn-icon btn-sm ms-3 btn btn-outline-gray-400"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="1em" height="1em" fill="currentColor" class="bi bi-text-indent-left fs-3"><path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m.646 2.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L4.293 8 2.646 6.354a.5.5 0 0 1 0-.708M7 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m-5 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"></path></svg>
+                            </button>
+                        </div>
+                        <ul class="account_tabs nav nav-tabs dropdown-menu" role="menu" aria-labelledby="menu1">
+                            <li class="d-inline-block px-1 m-1">
+                                <a href="#basicinfo" id="tab_basicinfo" data-bs-toggle="tab" class="active"><i class="fa fa-user me-2"></i>Basic Info</a></li>
+                            <li class="d-inline-block px-1 m-1"><a href="#changepassword" id="tab_changepassword" data-bs-toggle="tab"><i class="fa fa-gear me-2"></i>Change Password</a></li>
+                            <li class="d-inline-block px-1 m-1"><a href="#rentals" data-bs-toggle="tab" id="tab_rentals"><i class="fa fa-book me-2"></i>Rentals</a></li>
+                            <li class="d-inline-block px-1 m-1"><a href="<?=base_url('/Auth/signoff')?>"><i class="fa fa-sign-out-alt me-2"></i>Logout</a></li>
+                        </ul>
                     </div>
                     <div class="col-xl-9 col-md-8 col-sm-12 mb-2 ">
                         <div class="product-details-tab-content border">
@@ -60,7 +76,7 @@
                                         <div class="row">
                                             <div class="col-xl-8 col-md-12 col-sm-12">
                                                 <form class="form-control" id="update-password" method="POST" action="<?=base_url('Auth/changepassword')?>">
-                                                    <div class="col-xl-12 col-md-12 col-sm-6">
+                                                    <div class="col-xl-12 col-md-12 col-sm-12">
                                                         <div class="input-field">
                                                             <label for="cp">Current Password</label>
                                                             <input type="password" class="form-control" autocomplete="off" id="cp" name="current_password" value="" required>
@@ -93,7 +109,11 @@
                                         <div class="row g-4">
                                             <span class="text-dark m-2 p-2">No records found.</span>                                    
                                         </div>
-                                        <?php } else { ?>
+                                        <?php } else { 
+
+                                        $mobile = checkMobile();
+                                        if( $mobile == false ) {
+                                        ?>
                                         <div class="row g-4">
                                             <div class="col-12 overflow-auto">
                                                 <?php $subtotal = 0;$gst = 0;$total = 0;?>
@@ -171,7 +191,88 @@
                                                 </div>
                                             </div>                                                                            
                                         </div>
-                                        <?php } ?>
+                                        <?php } else { ?>
+                                        <!-- Mobile View -->
+                                       <div class="row g-4">
+                                            <div class="col-12 overflow-auto">
+                                                <?php $subtotal = 0;$gst = 0;$total = 0;?>
+                                                <div style="min-width: 800px;" class="table table-responsive bg-white rounded">
+                                                    <table class="table table-bordered">
+                                                        <thead>
+                                                            <tr class="bg-primary">
+                                                                <th style="width:5%;">#</th>
+                                                                <th>Quantity</th>
+                                                                <th>From</th>
+                                                                <th>To</th>
+                                                                <th>Total</th>
+                                                                <th>Refund Amount</th>
+                                                                <th>Paid</th>
+                                                                <th>Status</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        <?php foreach($rentals as $row) { 
+
+                                                              $bikes_ordered = array();
+                                                              $bikes_order = "";
+                                                              $bk = explode(",", $row['bikes_types']);
+                                                              $bk_qty = explode(",", $row['bikes_qty']);
+                                                              foreach($bk as $index => $bky)
+                                                              {
+                                                                if( isset( $bikes_ordered[ $biketypes[$bky] ] ) )
+                                                                {
+                                                                  $bikes_ordered[ $biketypes[$bky] ] = $bikes_ordered[ $biketypes[$bky] ] + $bk_qty[$index];
+                                                                }
+                                                                else
+                                                                {
+                                                                  $bikes_ordered[ $biketypes[$bky] ] = $bk_qty[$index];
+                                                                }
+                                                              }
+
+                                                              foreach($bikes_ordered as $name => $qty)
+                                                              {
+                                                                $bikes_order .= ( $bikes_order == "" ) ? "<p class='d-inline-block m-1  fa-sm fw-bold'>".$name."(".$qty.")</p>" : ",<p class='d-inline-block m-1 fa-sm fw-bold'>".$name."(".$qty.")</span>";
+                                                              }
+                                                        ?>
+                                                        <tr>
+                                                            <td>
+                                                                <?=$row['id']?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$bikes_order?>
+                                                                <?php if( $row['helmet_quantity'] != 0) {?>
+                                                                <p class="d-block mt-2 fa-sm fw-bold">Helmets(<?=$row['helmet_quantity']?>)</p>
+                                                                <?php } ?>
+                                                            </td>
+                                                            <td>
+                                                                <span class="m-2 p-2 fa-sm d-inline"><b><?=date("d M Y", strtotime($row['pickup_date']))?>&nbsp;<?=$row['pickup_time']?></b></span>
+                                                            </td>
+                                                            <td>
+                                                                <span class=" m-2 p-2 fa-sm d-inline"><b><?=date("d M Y", strtotime($row['dropoff_date']))?>&nbsp;<?=$row['dropoff_time']?></b></span>
+                                                            </td>
+                                                            <td class="text-nowrap"><i class="fa fa-indian-rupee-sign me-1"></i><?=$row['total_amount']?></td>
+                                                            <td class="text-nowrap"><i class="fa fa-indian-rupee-sign me-1"></i><?=$row['refund_amount']?></td>
+                                                            <td class="text-nowrap"><i class="fa fa-indian-rupee-sign me-1"></i><?=$row['booking_amount']?></td>
+                                                            <td>
+                                                                <?php if( $row['status'] == 0 ){ ?>
+                                                                    <span class="text-white p-1 px-2 border rounded-5 bg-warning font-sm text-nowrap">Pre Booked</span>
+                                                                <?php } elseif( $row['status'] == 1 ){ ?>
+                                                                    <span class="text-white p-1 px-2 border rounded-5 bg-success font-sm">Rented</span>
+                                                                <?php }
+                                                                else { ?>
+                                                                    <span class="text-white p-1 px-2 border rounded-5 bg-info font-sm">Closed</span>
+                                                                <?php } ?>                                                                        
+                                                            </td>
+                                                        </tr>
+                                                        <?php } ?>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>                                                                            
+                                        </div>
+                                        <?php }
+
+                                        } ?>
                                     </div>
                                 </div>
                             </div>
@@ -187,19 +288,15 @@
 
             $(document).ready(function(){
 
-                /*$(".account_tabs").find("a.btn_link").each(function(){
-                    $(this).removeClass('active');
+                $(".dropdown .dropdown-menu-toggle").click(function(){
+
+                    $(".dropdown .dropdown-menu").slideToggle("fast");
+
                 });
 
-                $(".account_tabs").find("a.btn_link").eq(0).addClass('active');*/
-
-                /*$(".account_tabs a.btn_link").click(function(){
-
-                    $(".account_tabs").find("a.btn_link.active").eq(0).removeClass('active');
-
-                    $(this).addClass('active');
-                    return true;
-                });*/
+                $(".dropdown-menu a").click(function(){
+                    $(".dropdown .dropdown-menu").slideToggle("fast");
+                });
 
             });
 
